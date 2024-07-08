0b304868b6214dbf3a3c1adf9d027d4f6f78c037
jaysonedu <jasonqinedu@gmail.com>
2024-07-08 15:55:54 -0400

    experiment with Bootstrap cards and projects

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projects.apps.ProjectsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
