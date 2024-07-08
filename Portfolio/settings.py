bdcbaea07d41f82a2ee7ed83d245993a1b8a1393
jaysonedu <jasonqinedu@gmail.com>
2024-07-08 16:09:02 -0400

    added images to template

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -115,6 +115,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = BASE_DIR / "uploads/"
+MEDIA_URL = "media/"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
