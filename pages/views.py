eff06819f1dcf387d94fc2994a795569e56147bb
jaysonedu <jasonqinedu@gmail.com>
2024-07-11 11:37:03 -0400

    pages update

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 # 7/8 first edit to views
 def home(request):
     return render(request, "pages/home.html", {})
+
