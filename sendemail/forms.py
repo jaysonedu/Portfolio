ae22068cad8c8676db234deaab7c2eab24067f53
jaysonedu <jasonqinedu@gmail.com>
2024-07-18 04:58:49 -0400

    edited design

--- sendemail/forms.py
+++ sendemail/forms.py
@@ -3,10 +3,10 @@ from django import forms
 
 
 class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Your name"}))
     email = forms.EmailField(
         widget=forms.TextInput(attrs={"placeholder": "Your e-mail"})
     )
-    subject = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Subject"}))
     message = forms.CharField(
         widget=forms.Textarea(attrs={"placeholder": "Your message"})
     )
