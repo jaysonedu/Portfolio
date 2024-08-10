5ff8161a59b8017ed71e5767a22bb783fe2a3305
jaysonedu <155124915+jaysonedu@users.noreply.github.com>
2024-08-10 17:05:24 -0400

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -36,6 +36,8 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+
 ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
 
 
