2841b4260630f24d0af602999c1c4ac3f311553b
jaysonedu <jasonqinedu@gmail.com>
2024-08-10 02:20:29 -0400

    started js

--- static/js/home.js
+++ static/js/home.js
@@ -1,31 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll(".project-section");
 
-    document.addEventListener("wheel", function(event) {
-        event.preventDefault();
 
-        const delta = Math.sign(event.deltaY);
+window.onscroll = () => {
+    let header = document.querySelector('header');
 
-        if (delta > 0) {
-            scrollToNextSection();
-        } else {
-            scrollToPrevSection();
-        }
-    });
-
-    let currentSectionIndex = 0;
-
-    function scrollToNextSection() {
-        if (currentSectionIndex < sections.length - 1) {
-            currentSectionIndex++;
-            sections[currentSectionIndex].scrollIntoView({ behavior: "smooth" });
-        }
-    }
-
-    function scrollToPrevSection() {
-        if (currentSectionIndex > 0) {
-            currentSectionIndex--;
-            sections[currentSectionIndex].scrollIntoView({ behavior: "smooth" });
-        }
-    }
-});
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
