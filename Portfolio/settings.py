ef8d11a3ae7d15afb48d9103e29cfefcfc4f128b
jaysonedu <jasonqinedu@gmail.com>
2024-08-10 16:32:41 -0400

    started server

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -19,11 +19,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+# Support env variables from .env file if defined
+import os
+from dotenv import load_dotenv
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r9+ia%apt^9!y0dky41ia=*bf=cr7z#lew*z)g$)h1c!mk0g_a'
+# SECRET_KEY = 'django-insecure-r9+ia%apt^9!y0dky41ia=*bf=cr7z#lew*z)g$)h1c!mk0g_a'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,6 +141,7 @@ NOTIFY_EMAIL = "jasonqinedu@gmail.com"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -138,3 +151,19 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
+
+STORAGES = {
+    # ...
+    'staticfiles': {
+        "BACKEND":
+"whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Creation Date: 7/8/2024
 About: Personal Portfolio using Django and Bootstrap with CSS and HTML.
 
 Source: https://realpython.com/get-started-with-django-1/
+Deploy tutorial: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Deployment
+
+Requirements:
+Django
+dj-database-url
+gunicorn
+psycopg2-binary
+wheel
+whitenoise
+python-dotenv
+
