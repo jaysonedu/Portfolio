7753ca95b0e75c4c74c17c019203451ac644d8a1
jaysonedu <jasonqinedu@gmail.com>
2024-08-12 16:35:34 -0400

    second fix to postfix

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -160,8 +160,6 @@ import dj_database_url
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': {
-            dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=1800)
-        } 
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=1800)
     }
 
