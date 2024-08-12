94da5dad42bd851008a89957d296782ba88541cf
jaysonedu <jasonqinedu@gmail.com>
2024-08-12 16:32:04 -0400

    fix to postgres

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -159,8 +159,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
+    DATABASES = {
+        'default': {
+            dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=1800)
+        } 
+    }
 
