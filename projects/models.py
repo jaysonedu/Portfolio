8358d9b8986c319593891dde8b018ab760d3c54e
jaysonedu <jasonqinedu@gmail.com>
2024-08-15 11:42:19 -0400

    fixed projects

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='projects/')
-    technology = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, blank=True)
+    location = models.TextField(blank=True)
+    timeframe = models.TextField(blank=True)
+    overview = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+
+
+    technology = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
