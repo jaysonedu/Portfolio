f5a85ffb344eb3d4d25e7fdd9099d1b0cfcb0614
jaysonedu <jasonqinedu@gmail.com>
2024-08-15 21:04:27 -0400

    added navbar background

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -1,11 +1,5 @@
-
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-
 window.onscroll = () => {
+
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
